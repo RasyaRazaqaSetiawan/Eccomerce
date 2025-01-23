139a1e3dd172bd7a688ef0a2708880c201d3afe6
Rasya <rasyarazaqasetiawan@gmail.com>
2025-01-23 23:41:16 +0700

    Create Route role for admin & user

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,19 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Middleware\IsAdmin;
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+// Route Admin(Backend)
+Route::group(['prefix' => 'admin', 'middleware' => ['auth', IsAdmin::class]], function () {
+    Route::get('/', function () {
+        return view('home');
+    });
+    // untuk Route Backend Lainnya
+});
+
 
 Route::get('/', function () {
     return view('welcome');
@@ -8,4 +21,3 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
